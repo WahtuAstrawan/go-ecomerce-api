064ee119ca5b6fef0fbc9edb4317532c596f7e7a
Wahtu Astrawan <chatt699@gmail.com>
2024-04-15 10:01:14 +0800

    feat: adding user register

---
+++ types/types.go
@@ -0,0 +1,27 @@
+package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+}
